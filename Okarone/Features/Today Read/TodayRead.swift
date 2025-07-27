c6a7d4731aa994cc4963a6c72b97def8b0baffa9
Sourav Ganguly <souravganguly.bd@gmail.com>
2025-07-28 00:42:12 +0600

    Add Bengali poem to TodayRead view and update text formatting
    - Introduced a Bengali poem by Bonolota Sen in the TodayRead view.
    - Replaced placeholder text with the poem, enhancing the content displayed.
    - Updated text formatting for improved readability, including line spacing and alignment adjustments.

--- Okarone/Features/Today Read/TodayRead.swift
+++ Okarone/Features/Today Read/TodayRead.swift
@@ -8,6 +8,35 @@
 import SwiftUI
 
 struct TodayRead: View {
+
+    let text =
+"""
+   **বনলতা সেন**
+
+হাজার বছর ধরে আমি পথ হাঁটিতেছি পৃথিবীর পথে,
+সিংহল সমুদ্র থেকে নিশীথের অন্ধকারে মালয় সাগরে
+অনেক ঘুরেছি আমি; বিম্বিসার-অশােকের ধূসর জগতে
+সেখানে ছিলাম আমি; আরাে দূর অন্ধকারে বিদর্ভ নগরে;
+আমি ক্লান্ত প্রাণ এক, চারিদিকে জীবনের সমুদ্র সফেন,
+আমারে দু-দণ্ড শান্তি দিয়েছিল নাটোরের বনলতা সেন।
+
+
+চুল তার কবেকার অন্ধকার বিদিশার নিশা,
+মুখ তার শ্রাবন্তীর কারুকার্য; অতি দূর সমুদ্রের ’পর
+হাল ভেঙে যে নাবিক হারায়েছে দিশা
+সবুজ ঘাসের দেশ যখন সে চোখে দেখে দারুচিনি-দ্বীপের ভিতর,
+তেমনি দেখেছি তারে অন্ধকারে, বলেছে সে, ‘এতদিন কোথায় ছিলেন?’
+পাখির নীড়ের মতাে চোখ তুলে নাটোরের বনলতা সেন।
+
+
+সমস্ত দিনের শেষে শিশিরের শব্দের মতন
+সন্ধ্যা আসে; ডানার রৌদ্রের গন্ধ মুছে ফেলে চিল;
+পৃথিবীর সব রঙ নিভে গেলে পাণ্ডুলিপি করে আয়ােজন
+তখন গল্পের তরে জোনাকির রঙে ঝিলমিল;
+সব পাখি ঘরে আসে—সব নদী—ফুরায় এ-জীবনের সব লেনদেন;
+থাকে শুধু অন্ধকার, মুখােমুখি বসিবার বনলতা সেন।
+"""
+
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 16) {
@@ -16,16 +45,12 @@ struct TodayRead: View {
                     .fontWeight(.bold)
                     .padding(.horizontal)
                 
-                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+                Text(LocalizedStringKey(text))
                     .padding(.horizontal)
-                
-                Text("Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                    .padding(.horizontal)
-                
-                Text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.")
-                    .padding(.horizontal)
-                
-                Text("Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.")
+                    .font(.body)
+                    .foregroundColor(.primary)
+                    .multilineTextAlignment(.leading)
+                    .lineSpacing(1.2)
                     .padding(.horizontal)
             }
             .padding(.vertical)
